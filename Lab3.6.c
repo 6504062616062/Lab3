5b93c8511c82fbf2d29611a08d84da5fe27b5649
Khemika Yutthanawarakul <153588865+6504062616062@users.noreply.github.com>
2025-12-15 16:20:04 +0700

    Add files via upload

---
+++ Lab3.6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+	int score;
+	scanf("%d",&score);
+	if (score >= 68) {
+	
+		if (score >= 85){
+		printf("A\n");}
+			
+		else if (score >= 75){
+		printf("B\n");}
+			
+		else  {
+			printf("C\n");
+			}
+		
+	// A B C
+} else if (score >= 55) {
+	printf("D\n");}
+else{
+	printf("F\n");
+	
+}
+	
+// D F
+}
+
+
+
